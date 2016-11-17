78a271627c2494553d708f07381d6ed4affa1854
Dario Cangialosi <dario.cangialosi@gmail.com>
2016-11-17 19:40:55 +0100

    function moved in related js file. link for 'retrieve password' in place of a button

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function logged_out(){
+$.get("who_is_logged.php")
+	.done(function(logged_user) {
+	if(""!=logged_user){
+		location="logged_in";
+	}else{
+		// fragment is ready
+		$("#logged_info").show();
+	}
+	})
+	.fail(failure);
+}
+
+function logged_in(){
+	$.get("who_is_logged.php")
+	.done(function(logged_user) {
+	if(""!=logged_user){
+		$("#who_is_logged").text(logged_user);
+		// fragment is ready
+		$("#logged_info").show();
+	}else{
+		location="logged_out";
+	}
+	})
+	.fail(failure);
+}
+
 function failure(){
 	alert("operation failed. please retry.");
+}
+
+function logout(){
+	$.get("ajax.php?action=logout").always(logged_in);
 }
