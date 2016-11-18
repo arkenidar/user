1f20b8aa50170e910862c3ffd912a5aa7e026e63
Dario Cangialosi <dario.cangialosi@gmail.com>
2016-11-18 15:51:41 +0100

    moved functions to common file, uf(), no ajax deferreds, jquery in place of zepto (again)

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+$.ajaxSetup({error: failure});
+
 function logged_out(){
-$.get("who_is_logged.php")
-	.done(function(logged_user) {
+$.get(uf("who_is_logged"), function(logged_user) {
 	if(""!=logged_user){
 		location="logged_in";
 	}else{
 		// fragment is ready
 		$("#logged_info").show();
 	}
-	})
-	.fail(failure);
+});
 }
 
 function logged_in(){
-	$.get("who_is_logged.php")
-	.done(function(logged_user) {
+$.get(uf("who_is_logged"), function(logged_user) {
 	if(""!=logged_user){
 		$("#who_is_logged").text(logged_user);
 		// fragment is ready
@@ -21,8 +20,7 @@ function logged_in(){
 	}else{
 		location="logged_out";
 	}
-	})
-	.fail(failure);
+});
 }
 
 function failure(){
@@ -30,5 +28,35 @@ function failure(){
 }
 
 function logout(){
-	$.get("ajax.php?action=logout").always(logged_in);
+	$.get(uf("ajax")+"?action=logout", logged_in);
+}
+
+// url filename
+function uf(filename){
+	return filename+".php";
+}
+
+function login_submit(event){
+	$("#message").text( "Loading..." );
+	$.post(uf("ajax")+"?action=login", $("#login").serialize(),
+	function(data) {
+		$("#message").text( data.message.text );
+		if(data.message.status=="login successful") location = "logged_in";
+	});
+	return false;
+}
+
+function retrieve_password(){
+	$.post(uf("ajax")+"?action=retrieve_password", $("#login").serialize(),
+	function(data) {
+		$("#message").text( data.message );
+	});
+}
+
+function register_submit(event){
+	$.post(uf("ajax")+"?action=register", $("#register").serialize(),
+	function(data) {
+		$("#message").text( data.message );
+	});
+	return false;
 }
