9f17816f511ced558e3232e603b0215ee80d017d
Dario Cangialosi, Coder <dario.cangialosi@gmail.com>
2017-06-20 20:46:43 +0200

    syntax style changes in script.js

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-$.ajaxSetup({error: failure});
+$.ajaxSetup({error: failure})
 
 function logged_out(){
-$.get(uf("who_is_logged"), function(logged_user) {
-	if(""!=logged_user){
-		location="logged_in.html";
+$.get(uf('who_is_logged'), function(logged_user) {
+	if(''!=logged_user){
+		location='logged_in.html'
 	}else{
 		// fragment is ready
-		$("#logged_info").show();
+		$('#logged_info').show()
 	}
-});
+})
 }
 
 function logged_in(){
-$.get(uf("who_is_logged"), function(logged_user) {
-	if(""!=logged_user){
-		$("#who_is_logged").text(logged_user);
+$.get(uf('who_is_logged'), function(logged_user) {
+	if(''!=logged_user){
+		$('#who_is_logged').text(logged_user)
 		// fragment is ready
-		$("#logged_info").show();
+		$('#logged_info').show()
 	}else{
-		location="logged_out.html";
+		location='logged_out.html'
 	}
-});
+})
 }
 
 function failure(){
-	alert("operation failed. please retry.");
+	alert('operation failed. please retry.')
 }
 
 function logout(){
-	$.get(uf("ajax")+"?action=logout", logged_in);
+	$.get(uf('ajax')+'?action=logout', logged_in)
 }
 
 // url filename
 function uf(filename){
-	return filename+".php"; // you can change this when back-end is not PHP-based
+	return filename+'.php' // you can change this when back-end is not PHP-based
 }
 
 function login_submit(){
-	$("#message").text( "Loading..." );
-	$.post(uf("ajax")+"?action=login", $("#login").serialize(),
+	$('#message').text( 'Loading...' )
+	$.post(uf('ajax')+'?action=login', $('#login').serialize(),
 	function(data) {
-		$("#message").text( data.message.text );
-		if(data.message.status=="login successful") location = "logged_in.html";
-	});
+		$('#message').text( data.message.text )
+		if(data.message.status=='login successful') location = 'logged_in.html'
+	})
 }
 
 function retrieve_password(){
-	$.post(uf("ajax")+"?action=retrieve_password", $("#login").serialize(),
+	$.post(uf('ajax')+'?action=retrieve_password', $('#login').serialize(),
 	function(data) {
-		$("#message").text( data.message );
-	});
+		$('#message').text( data.message )
+	})
 }
 
 function register_submit(){
-	$.post(uf("ajax")+"?action=register", $("#register").serialize(),
+	$.post(uf('ajax')+'?action=register', $('#register').serialize(),
 	function(data) {
-		$("#message").text( data.message );
-	});
+		$('#message').text( data.message )
+	})
 }
